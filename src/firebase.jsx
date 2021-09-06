21c29e9c0bd590b720da5e6ca0fbe89a622e9d75
shreyas_barve <59538753+shreyasbarve@users.noreply.github.com>
2021-09-06 10:44:10 +0530

    Update firebase.jsx

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -4,14 +4,14 @@ import "firebase/firestore";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDG9dtoC0iwj1TNB04cjUJKqM_BRg9erqU",
-  authDomain: "disease-predictor-e446f.firebaseapp.com",
-  databaseURL: "https://disease-predictor-e446f-default-rtdb.firebaseio.com",
-  projectId: "disease-predictor-e446f",
-  storageBucket: "disease-predictor-e446f.appspot.com",
-  messagingSenderId: "459690692589",
-  appId: "1:459690692589:web:e7e6319809b601945c61e4",
-  measurementId: "G-NVNZP0ZQC8",
+  apiKey: "Your API key",
+  authDomain: "Your authDomain",
+  databaseURL: "Your databaseURL",
+  projectId: "Your projectId",
+  storageBucket: "Your storageBucket",
+  messagingSenderId: "Your messagingSenderId",
+  appId: "Your appId"
+  measurementId: "Your measurementId",
 };
 
 firebase.initializeApp(firebaseConfig);
